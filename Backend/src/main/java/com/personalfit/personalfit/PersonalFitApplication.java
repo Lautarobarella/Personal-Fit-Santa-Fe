bd7fde2546a0546242a7a3e8a4abe2a0ed7e2e48
Tomas Haulet <tomshaulet@gmail.com>
2025-07-15 13:04:30 -0300

    backend inicial

---
+++ Backend/src/main/java/com/personalfit/personalfit/PersonalFitApplication.java
@@ -0,0 +1,13 @@
+package com.personalfit.personalfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalFitApplication.class, args);
+    }
+
+}
