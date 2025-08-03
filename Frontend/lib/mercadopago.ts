f0b0eaab023edd8affa1ab8e46f918ef1d94049f
FerAleUtnSantaFe <fernandoale00@hotmail.com>
2025-08-03 15:04:18 -0300

    test

--- Frontend/lib/mercadopago.ts
+++ Frontend/lib/mercadopago.ts
@@ -13,6 +13,10 @@ import { getProductById } from "./products";
  */
 const client = new MercadoPagoConfig({
     accessToken: process.env.MP_ACCESS_TOKEN!,
+    options: {
+        timeout: 5000,
+        idempotencyKey: 'abc'
+    }
 });
 
 // Instancia de Preference para crear preferencias de pago
